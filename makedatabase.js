da2350e7227852808af8e928ec4cfc7c73b82d70
RoseChilds <rosechilds09@gmail.com>
2021-04-08 19:16:32 +0100

    add logging incase error

--- makedatabase.js
+++ makedatabase.js
@@ -4,5 +4,6 @@ var db = new sqlite3.Database('data.db');
 db.serialize(function() {
   db.run("CREATE TABLE leaderboard (id INTEGER PRIMARY KEY AUTOINCREMENT, userid TEXT, username TEXT, score INTEGER, time INTEGER)", function(data){
     console.log("created table leaderboard");
+    console.log(data);
   });
 });
